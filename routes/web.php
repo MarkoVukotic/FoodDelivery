3b0da083215181442346f6b5db66c78ac0314cdd
marko.v <markovukotic32@gmail.com>
2023-02-04 08:52:07 +0100

    Login and SingUp page

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,13 @@ Route::get('/', function () {
 });
 
 Route::resource('/food', FoodController::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
